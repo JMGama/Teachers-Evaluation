110d5d4f41f93d710014bdcfebf5613792ad197b
Jose Manuel Gama Estrada <josegamaes@gmail.com>
2018-10-25 11:25:08 -0500

    Teacher Info and Signatures.

--- evaluations/views/teacher_evaluation.py
+++ evaluations/views/teacher_evaluation.py
@@ -1,8 +1,22 @@
+from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
 from .general_functions import *
 
+
 class TeacherEvaluationView(View, GeneralFunctions):
+
+    template_teacher_evaluation = 'evaluations/teacher_evaluation.html'
+    template_login = 'evaluations/login.html'
+
     def get(self, request):
+        if not request.session.get('session', False) or not request.session['type'] == 'teacher':
+            return render(request, self.template_login)
+
+        teacher = EvaluationsTeachers.objects.get(
+            idperson__exact=request.session['id_teacher'])
+        signatures = EvaluationsSignatures.objects.filter(pk__in=EvaluationsDetailGroupPeriodSignature.objects.filter(
+            idteacher__exact=teacher.idperson).values_list('idsignature', flat=True))
+        print(signatures)
         return HttpResponse("WELCOME TO THE TEACHERS VIEW")
