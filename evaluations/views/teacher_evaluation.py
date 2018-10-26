489033d4743af8643aaa9b9f980d38cbe8d77104
Jose Manuel Gama Estrada <josegamaes@gmail.com>
2018-10-26 12:17:37 -0500

    Getting Teacher Evaluated Signatures

--- evaluations/views/teacher_evaluation.py
+++ evaluations/views/teacher_evaluation.py
@@ -18,9 +18,21 @@ class TeacherEvaluationView(View, GeneralFunctions):
             idperson__exact=request.session['id_teacher'])
         signatures_detail = EvaluationsDetailGroupPeriodSignature.objects.filter(
             idteacher__exact=teacher.idperson)
-
-
+        evaluated_signatures = self.get_teacher_eval_signatures(
+            teacher, signatures_detail)
+        print(evaluated_signatures)
         return HttpResponse("WELCOME TO THE TEACHERS VIEW")
 
-    def get_teacher_eval_signatures(self, teacher):
-        pass
+    def get_teacher_eval_signatures(self, teacher, signatures_detail):
+        """return a list of all the evaluations (groupid) already made by the teacher"""
+        teacher_exams = EvaluationsExams.objects.filter(
+            Q(type='DOCENTES') & Q(status__exact='ACTIVO'))
+
+        result = {}
+        for exam in teacher_exams:
+            eval_signatures = []
+            for signature_dtl in signatures_detail:
+                eval_signatures.append(EvaluationsDetailTeacherSignatureExam.objects.filter(
+                    idexam__exact=exam, idgroup__exact=signature_dtl.idgroup, idsignature__exact=signature_dtl.idsignature))
+            result[exam]= eval_signatures
+        return result
