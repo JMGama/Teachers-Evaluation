01244fd2ed1cc9ab1871e907fff9f35aca275fe4
Jose Manuel Gama Estrada <josegamaes@gmail.com>
2018-11-14 16:06:26 -0600

    Switch for Different Reports

--- evaluations/views/teacher_reports.py
+++ evaluations/views/teacher_reports.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from easy_pdf.rendering import render_to_pdf_response
 from .general_functions import *
 
 class TeacherReports(View, GeneralFunctions):
-    def get(self, request, career_id, teacher_id):
-        pass
+    template_login = 'evaluations/login.html'
+
+    def get(self, request, report_type):
+        """Function that make the reports"""
+        if not request.session.get('session', False) or not request.session['type'] == 'coordinator':
+            return render(request, self.template_login)
+
+        if report_type == 'all_teachers_report':
+            pass
+        elif report_type =='all_teachers_report':
+            pass
+        elif report_type == 'teacher_report':
+            pass
+        elif report_type == 'career_teachers_report':
+            pass
+
+        return redirect('monitoring/')
