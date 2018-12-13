1da511f22aaf73a21196cdc5af7c2d32838447fd
Jose Manuel Gama Estrada <josegamaes@gmail.com>
2018-12-13 13:14:28 -0600

    comments and sort logout

--- evaluations/views/logout.py
+++ evaluations/views/logout.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from django.views import View
 from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
 
 from evaluations.models import *
+
 
 class LogoutView(View, ):
     template_login = 'evaluations/login.html'
 
     def get(self, request):
+        """Flush the session and redirects to the login page"""
         try:
             request.session.flush()
         except KeyError:
